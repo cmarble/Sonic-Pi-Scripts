4adb30bd2a1855acf0a0f15ecff6f4b1e681e8e5
cmarble <camryn_marble@davincischools.org>
2018-03-26 15:44:20 -0700

    Add files via upload

---
+++ melody.rb
@@ -0,0 +1,37 @@
+use_bpm 200
+use_synth :piano
+
+
+play :a3
+sleep 0.5
+play :c4
+sleep 0.5
+play :d4
+sleep 1
+play :d4
+sleep 1
+play :d4
+sleep 0.5
+play :e4
+sleep 0.5
+play :f4
+sleep 1
+play :f4
+sleep 1
+play :f4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :a3
+sleep 0.5
+play :c4
+sleep 0.5
+play :d4
+sleep 1
+play :d4
+sleep 1
+
