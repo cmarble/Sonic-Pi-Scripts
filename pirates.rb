fc2c53ae67f71d48895b16a57c7a89b8d70ec89a
cmarble <camryn_marble@davincischools.org>
2018-03-27 14:05:36 -0700

    Update pirates.rb

--- pirates.rb
+++ pirates.rb
@@ -1,5 +1,8 @@
+x = 200
 
-use_bpm 200
+use_bpm x
+
+
 
 define :beginning do |note1|
   play note1
@@ -17,7 +20,8 @@ define :quarter do |note1, note2, note3|
   sleep 0.5
 end
 
-live_loop :piano do
+
+live_loop :piano dos
   use_synth :piano
   7.times do
     beginning :d3 #piano
@@ -32,11 +36,9 @@ live_loop :piano do
   
   quarter :g3, :f3, :a3 #piano
   
-  3.times do #piano
-    beginning :d3
-  end
   
-  quarter :d3, :a3, :c4 #piano
+  
+  stop
 end
 
 live_loop :cello do
@@ -55,12 +57,142 @@ live_loop :cello do
   
   quarter :d2, :d2, :d2
   
-  3.times do
-    beginning :d2
-  end
   
-  quarter :d2, :d2, :d2
+  
+  stop
 end
+live_loop :melody1 do
+  use_synth :piano
+  sleep 23.5
+  sleep 0.5
+  play :d4
+  sleep 1
+  play :d4
+  sleep 1
+  play :d4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :f4
+  sleep 1
+  play :f4
+  sleep 1
+  play :f4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  sleep 1
+  play :e4
+  sleep 1
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :d4
+  sleep 1
+  play :a3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :d4
+  sleep 1
+  play :d4
+  sleep 1
+  play :d4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :f4
+  sleep 1
+  play :f4
+  sleep 1
+  play :f4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  sleep 1
+  play :e4
+  sleep 1
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :d4, sustain: 3
+  sleep 3
+  stop
+end
+
+live_loop :melody2 do
+  use_synth :fm
+  sleep 23.5
+  sleep 0.5
+  play :d2
+  sleep 1
+  play :d2
+  sleep 1
+  play :d2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :f2
+  sleep 1
+  play :f2
+  sleep 1
+  play :f2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :d2
+  sleep 0.5
+  play :c2
+  sleep 0.5
+  play :c2
+  sleep 0.5
+  play :d2
+  sleep 1
+  
+  play :a1
+  sleep 0.5
+  play :c2
+  sleep 0.5
+  play :d2
+  sleep 1
+  play :d2
+  sleep 1
+  play :d2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :f2
+  sleep 1
+  play :f2
+  sleep 1
+  play :f2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 1
+  play :e2
+  sleep 1
+  play :d2
+  sleep 0.5
+  play :c2
+  sleep 0.5
+  play :d2, sustain: 3
+  sleep 3
+  stop
+end
+
+
 
 
 
